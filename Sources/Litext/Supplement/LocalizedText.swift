7961a6ead2f5a1496af2dc743931fcd1a354eaa0
Gary Tokman <yo@garytokman.com>
2026-04-08 17:10:59 -0400

    fix: bundle crash at runtime

--- Sources/Litext/Supplement/LocalizedText.swift
+++ Sources/Litext/Supplement/LocalizedText.swift
@@ -14,11 +14,19 @@ import Foundation
 // **Localized resources can be mixed** -> true
 //
 
+private let localizationBundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return .module
+    #else
+    return .main
+    #endif
+}()
+
 public enum LocalizedText {
-    public static let copy = NSLocalizedString("Copy", bundle: .module, comment: "Copy menu item")
-    public static let selectAll = NSLocalizedString("Select All", bundle: .module, comment: "Select all menu item")
-    public static let share = NSLocalizedString("Share", bundle: .module, comment: "Share menu item")
-    public static let search = NSLocalizedString("Search", bundle: .module, comment: "Search index menu item")
-    public static let openLink = NSLocalizedString("Open Link", bundle: .module, comment: "Open link menu item")
-    public static let copyLink = NSLocalizedString("Copy Link", bundle: .module, comment: "Copy link menu item")
+    public static let copy = NSLocalizedString("Copy", bundle: localizationBundle, comment: "Copy menu item")
+    public static let selectAll = NSLocalizedString("Select All", bundle: localizationBundle, comment: "Select all menu item")
+    public static let share = NSLocalizedString("Share", bundle: localizationBundle, comment: "Share menu item")
+    public static let search = NSLocalizedString("Search", bundle: localizationBundle, comment: "Search index menu item")
+    public static let openLink = NSLocalizedString("Open Link", bundle: localizationBundle, comment: "Open link menu item")
+    public static let copyLink = NSLocalizedString("Copy Link", bundle: localizationBundle, comment: "Copy link menu item")
 }
